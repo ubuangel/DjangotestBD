bd0040dcdeb26afe371bcafc8351d9e12f61595c
angel <andresbjar97@gmail.com>
2020-12-10 23:21:57 -0500

    Después de que alguien vota en una pregunta, la vista vote () redirige a la página de resultados de la pregunta. Escribamos esa

--- Proyecto2/polls/views.py
+++ Proyecto2/polls/views.py
@@ -2,7 +2,7 @@
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts  import get_object_or_404,render
 from django.template  import loader
@@ -20,8 +20,9 @@ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
