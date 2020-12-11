c7f2ba0b542b8eb0c1d9da0c5812913fe745aa5b
angel <andresbjar97@gmail.com>
2020-12-10 23:35:31 -0500

    Use vistas genéricas: menos código es mejor
    1.Convierta el URLconf.
    
    2.Elimine algunas de las vistas antiguas innecesarias.
    
    3.Introduce nuevas vistas basadas en las vistas genéricas de Django.

--- Proyecto2/polls/views.py
+++ Proyecto2/polls/views.py
@@ -8,20 +8,31 @@ from django.shortcuts  import get_object_or_404,render
 from django.template  import loader
 from django.urls import reverse
 
+from django.views import generic
+
 from  .models  import Question,Choice
 #creando un averdares funcion vote() en template
 #devuelve un objet ohttpresponse 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 
 def vote(request, question_id):
