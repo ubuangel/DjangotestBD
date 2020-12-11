e2e534f1cae4a23998a719b5b8a783a544f59cf0
angel <andresbjar97@gmail.com>
2020-12-10 23:40:44 -0500

    Crea una prueba para exponer el error

--- Proyecto2/polls/tests.py
+++ Proyecto2/polls/tests.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 from .models import Question
 
-
+#
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -16,3 +16,8 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+        
+        
+        
+        
+        
